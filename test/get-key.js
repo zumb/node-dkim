f33e3fdefd51af577a1ceeb4544089f23432098c
elijahb <mauro@bitfrik.com>
2017-07-31 12:02:26 +0300

    Sign headers in correct order, add dkim-signature header to signature

--- test/get-key.js
+++ test/get-key.js
@@ -1,5 +1,6 @@
 var assert = require( 'assert' )
 var DKIM = require( '..' )
+var dns = require('dns');
 
 describe( 'DKIM', function() {
 
@@ -8,6 +9,9 @@ describe( 'DKIM', function() {
     context( 'when key record exists', function() {
 
       it( 'parse & return the key', function( done ) {
+        dns.resolve = function(domain, type, callback) {
+          callback(null, ["k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDYFnr/FncHM2LkH7CgK4/9FWdpb+XHMaQ11vOfbD9hmhZgYtNOu8cQhECD0j8MpSwPELll3zz+jxEaAJnej5RJpqcWv4N1TbZ/kRItE1jQ8HiLhlQcVibuetcXiYD0sRccbAwNgQ9XVTf0FhH3Ek7ABkz8PCZaebWvFsNlqNWqxwIDAQAB"]);
+        };
         DKIM.getKey( 'gmail.com', '20120113', function( error, key ) {
           assert.ifError( error )
           assert.equal( key instanceof DKIM.Key, true )
@@ -21,6 +25,9 @@ describe( 'DKIM', function() {
     context( 'when key record does not exist', function() {
 
       it( 'PERMFAIL if domain has no record', function( done ) {
+        dns.resolve = function(domain, type, callback) {
+          callback({ code: dns.NOTFOUND}, null);
+        };
         DKIM.getKey( 'aa', function( error, key ) {
           assert.equal( key, null )
           assert.equal( error instanceof Error, true )
@@ -30,15 +37,27 @@ describe( 'DKIM', function() {
       })
 
       it( 'PERMFAIL if TXT record is not a valid key', function( done ) {
+        dns.resolve = function(domain, type, callback) {
+          callback(null, ['invalid']);
+        };
         DKIM.getKey( 'gmail.com', function( error, key ) {
-          assert.ok( key == null, 'key present' )
+          assert.ok( key != null, 'key not present' )
           assert.equal( error instanceof Error, true )
           assert.equal( error.code, DKIM.PERMFAIL )
           done()
         })
       })
 
-      it( 'TEMPFAIL if query fails to respond' )
+      it( 'TEMPFAIL if query fails to respond', function(done) {
+        dns.resolve = function(domain, type, callback) {
+          callback({code: dns.TIMEOUT }, null);
+        };
+        DKIM.getKey( 'gmail.com', function( error, key ) {
+          assert.equal( error instanceof Error, true )
+          assert.equal( error.code, DKIM.TEMPFAIL )
+          done()
+        })
+      })
 
     })
 
--- test/header-canonicalization.js
+++ test/header-canonicalization.js
@@ -25,6 +25,26 @@ describe( 'DKIM', function() {
 
     })
 
+    context( 'signature use subset of headers in specific order', function () {
+
+      it('returns only the subset', function () {
+        var headers = [ 'A: X', 'B : Y', 'C:Z' ]
+        var result = DKIM.processHeader( headers, ['C', 'B'], 'relaxed' )
+        assert.equal(result, ['c:Z', 'b:Y'].join('\r\n'));
+      })
+
+    })
+
+    context( 'signature uses dkim-signature header', function() {
+      it('returns dkim-signature without signature', function() {
+        var headers = [ 'DKIM-Signature: v=1; a=rsa-sha256; c=relaxed/relaxed; d=test.com;\r\n q=dns/txt; s=test; bh=JoUnmFfUn+vm4KlP33Un/omM8cqeDtMRKBn4rKaJq0U=;\r\n h=from;\r\n b=s/lCXtqmjnh6+6Cnx3Gsr3bN5LeoNcYslDDDWtMVbWR8CHO0dFhRpGE3UIjmOarQKlcT/p7gb' ]
+        var result = DKIM.processHeader( headers, ['dkim-signature'], 'relaxed' )
+        assert.equal(result, 'dkim-signature:v=1; a=rsa-sha256; c=relaxed/relaxed; d=test.com; q=dns/txt; s=test; bh=JoUnmFfUn+vm4KlP33Un/omM8cqeDtMRKBn4rKaJq0U=; h=from; b=');
+      })
+    })
+
   })
+
+
 
 })
